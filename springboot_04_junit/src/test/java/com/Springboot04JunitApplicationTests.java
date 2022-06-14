2d72a19938facd03c434a6277f29520e19d6cfa5
Li, Diyang <dli487@gatech.edu>
2022-06-14 16:57:24 -0400

    Springboot use junit

--- springboot_04_junit/src/test/java/com/Springboot04JunitApplicationTests.java
+++ springboot_04_junit/src/test/java/com/Springboot04JunitApplicationTests.java
@@ -1,11 +1,14 @@
-package com.itheima.springboot_04_junit;
+package com;
 
+import com.itheima.Springboot04JunitApplication;
 import com.itheima.dao.BookDAO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Springboot04JunitApplication.class)
+
+
 class Springboot04JunitApplicationTests {
     @Autowired
     private BookDAO bookDAO;
