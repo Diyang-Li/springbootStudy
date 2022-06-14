79a3d5bcf2df54f098803c650a5ea93151af976b
Li, Diyang <dli487@gatech.edu>
2022-06-13 23:46:39 -0400

    Springboot use junit @Autowired

--- springboot_02_Configeration/src/main/java/com/itheima/controller/BookController.java
+++ springboot_02_Configeration/src/main/java/com/itheima/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.itheima.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    @Value("${country}")
+    private String country1;
     @GetMapping
     public String getById(){
         System.out.println("springboot is running1221...");
+        System.out.println(country1);
         return "springboot is running...12112";
     }
 }
